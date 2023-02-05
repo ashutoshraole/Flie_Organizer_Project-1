9777d322a6df006623da66d90aa3aba76dd55238
ashutoshraole <ashutoshraole@gmail.com>
2023-02-05 20:38:57 +0530

    sprint 1 complete

--- File_Organizer_Project_1/src/my_project_1/project_exe.java
+++ File_Organizer_Project_1/src/my_project_1/project_exe.java
@@ -5,7 +5,9 @@ public class project_exe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		welcome_screen ws=new welcome_screen();
+		ui u=new ui();
 		ws.printLetters(ws.w);
+		u.print_ui();
 	}
 
 }
