185c884f95cb13091809092d4d33d473ac3a8e0b
ashutoshraole <ashutoshraole@gmail.com>
2023-02-05 23:50:03 +0530

    sprint-2 revised

--- File_Organizer_Project_1/src/my_project_1/show_file.java
+++ File_Organizer_Project_1/src/my_project_1/show_file.java
@@ -1,9 +1,12 @@
 package my_project_1;
 import java.io.File;
 import java.util.Collections;
+import java.util.*;
 
 public class show_file {
 	void print_file() {
+		Scanner sc= new Scanner(System.in);
+		int back =0;
 		String folderpath="E:\\Full Stack Development\\Simplilearn\\COURSE-2\\Flie_Organizer_Project-1\\Files";
 		File folder=new File(folderpath);
 		File [] files =folder.listFiles(); 
@@ -14,6 +17,18 @@ public class show_file {
 			   System.out.println("* File ->"+" "+file.getName());
 			}
 
+		}
+		while (true) {
+		System.out.println("Enter [9] to back to Previous Menu");
+		back =sc.nextInt();
+			if(back==9) {
+			ui ui_obj=new ui();
+			ui_obj.print_ui();
+			break;
+			}
+			else {
+			System.out.println("Enter Valid Option");
+			}
 		}
 	}
 
